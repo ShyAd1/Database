37c61efb3fd9d5f051fe555a4dc926a56fe20515
Adair Hernandez <adheva05@gmail.com>
2025-06-19 13:04:10 -0600

    v1.8

--- practica6/funciones_fecha_ordenamiento_agrupamiento.sql
+++ practica6/funciones_fecha_ordenamiento_agrupamiento.sql
@@ -6,7 +6,8 @@ select current_date,date_add(current_date, interval 7 day) as fecha_dentro_de_un
 
 -- Muestra " el día de hoy [11/03/2010] es [Saturday] ".
 -- Donde lo que está en corchete se obtenga por comando SQL y no por texto.
-select concat('El dia de hoy [', date_format(current_date, '%d/%m/%Y'), '] es [', dayname(current_date), ']');
+select concat('El dia de hoy [', date_format(current_date, '%d/%m/%Y'), 
+'] es [', dayname(current_date), ']');
 
 -- Consultar todos los préstamos realizados en los últimos 7 días.
 select * from prestamos where fecha_prestamo >= current_date - interval 7 day;
@@ -157,16 +158,96 @@ and current_date group by libros.id_libro, libros.titulo order by veces_prestado
 
 -- Inciso c)
 -- ¿Cuántas personas estan registradas de cada rol tenemos?
+select id_rol, count(*) as cantidad_usuarios from usuarios group by id_rol;
+
 -- Obtener la cantidad de préstamos por estado de préstamo (pendiente, retraso, devuelto)
+select estado_prestamo, count(*) as cantidad_prestamos from prestamos group by estado_prestamo;
+
 -- Obtener el número total de libros prestados por editorial
+select libros.editorial, count(*) as libros_prestados_editorial from libros, prestamos
+where libros.id_libro = prestamos.id_libro group by libros.editorial;
+
 -- Calcular la multa promedio por usuario para préstamos con estado retraso
+select prestamos.estado_prestamo, avg(multas.monto) as multa_promedio from prestamos, multas
+where prestamos.id_prestamo = multas.id_prestamo and prestamos.estado_prestamo = 'con retraso'
+group by prestamos.estado_prestamo;
+insert into multas (id_prestamo, monto, pagado, fecha_multa) values
+(3, 50.00, 0, '2024-12-01'),
+(15, 25.50, 1, '2024-11-25'),
+(22, 10.00, 1, '2024-11-30'),
+(25, 15.75, 0, '2024-12-10'),
+(39, 20.00, 1, '2024-11-20'),
+(43, 5.00, 0, '2024-12-03'),
+(52, 35.00, 1, '2024-12-04'),
+(58, 60.00, 0, '2024-11-18'),
+(62, 40.00, 1, '2024-12-08'),
+(66, 18.00, 0, '2024-12-05');
+select prestamos.estado_prestamo, avg(multas.monto) as multa_promedio from prestamos, multas
+where prestamos.id_prestamo = multas.id_prestamo and prestamos.estado_prestamo = 'con retraso'
+group by prestamos.estado_prestamo;
+
 -- Mostrar el total de libros disponibles y prestados, agrupados por año de publicación
+select l.anio_publicacion, (select sum(cantidad_disponible) from libros l2 where 
+l2.anio_publicacion = l.anio_publicacion) as total_disponibles, count(p.id_prestamo) 
+as total_prestados from libros l, prestamos p where l.id_libro = p.id_libro
+group by l.anio_publicacion;
+
 -- Contar la cantidad de usuarios por rango de edad usando CASE y agrupar los resultados
+select case
+	when floor(datediff(current_date, date(fecha_nacimiento))/365.25) >= 45 then 'Tienen 45 o mas años' 
+    when floor(datediff(current_date, date(fecha_nacimiento))/365.25) >= 35 then 'Tienen entre 35 y 44 años' 
+	when floor(datediff(current_date, date(fecha_nacimiento))/365.25) >= 25 then 'Tienen entre 25 y 34 años'
+    else 'Tienen menos de 25 años'
+end as rango_edad, count(*) as catidad_usuarios from usuarios group by rango_edad;
+
 -- Calcular el promedio de días de retraso en devoluciones agrupado por el estado del préstamo
+select p.estado_prestamo, avg(datediff(d.fecha_devolucion, p.fecha_devolucion_max)) as 
+dias_promedio_retraso from devoluciones d, prestamos p
+where p.id_prestamo = d.id_prestamo group by p.estado_prestamo;
+
 -- Contar la cantidad de libros por clasificación (de 0 a 5 estrellas)
+select case
+	when clasificacion = 5 then 'Tienen una puntuacion de 5 estrellas'
+    when clasificacion = 4 then 'Tienen una puntuacion de 4 estrellas'
+	when clasificacion = 3 then 'Tienen una puntuacion de 3 estrellas'
+    when clasificacion = 2 then 'Tienen una puntuacion de 2 estrellas'
+    when clasificacion = 1 then 'Tienen una puntuacion de 1 estrellas'
+    when clasificacion = 0 then 'Tienen una puntuacion de 0 estrellas'
+    else 'No tiene puntuacion'
+end as puntuacion, count(*) as catidad_libros from libros group by puntuacion;
+
 -- Calcular el número de devoluciones por mes en el último año
+select mes_num, case mes_num
+     when 1 then 'Enero'
+     when 2 then 'Febrero'
+     when 3 then 'Marzo'
+     when 4 then 'Abril'
+     when 5 then 'Mayo'
+     when 6 then 'Junio'
+     when 7 then 'Julio'
+     when 8 then 'Agosto'
+     when 9 then 'Septiembre'
+     when 10 then 'Octubre'
+     when 11 then 'Noviembre'
+     when 12 then 'Diciembre'
+     else 'Desconocido'
+end as mes_nombre, numero_devoluciones_mes from(select month(fecha_devolucion) as mes_num,
+count(*) as numero_devoluciones_mes from devoluciones where fecha_devolucion 
+between date_sub(current_date, interval 1 year) and current_date group by month(fecha_devolucion)
+) as sub order by mes_num;
+
 -- Mostrar el número de libros prestados en los últimos 6 meses, agrupados por editorial
+select l.editorial, count(*) as libros_prestados from libros l, prestamos p
+where l.id_libro = p.id_libro and p.fecha_prestamo between 
+date_sub(current_date, interval 6 month) and current_date group by l.editorial;
+insert into prestamos (id_usuario, id_libro, fecha_prestamo, fecha_devolucion_max, estado_prestamo) values
+(12, 1, '2025-05-15', '2025-05-29', 'devuelto');
+select l.editorial, count(*) as libros_prestados from libros l, prestamos p
+where l.id_libro = p.id_libro and p.fecha_prestamo between 
+date_sub(current_date, interval 6 month) and current_date group by l.editorial;
+
 -- Calcular el número promedio de días que tarda en devolver los libros cada usuario
-
-
+select p.id_usuario, avg(greatest(datediff(d.fecha_devolucion, p.fecha_devolucion_max), 0)) as 
+dias_promedio_devolver from devoluciones d, prestamos p
+where p.id_prestamo = d.id_prestamo group by p.id_usuario;
 
