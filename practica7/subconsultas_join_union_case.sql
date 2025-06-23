bef647553bb3d70f18aa3a42c6d40e1dc12b7ab0
Adair Hernandez <adheva05@gmail.com>
2025-06-22 21:39:28 -0600

    v1.9

--- practica7/subconsultas_join_union_case.sql
+++ practica7/subconsultas_join_union_case.sql
@@ -117,14 +117,69 @@ values ('Sofía', 'Ramírez', 'López', 'sofia.ramirez@example.com', '5551230001
     ('María', 'Flores', 'Núñez', 'maria.flores@example.com', '5551230005', '1988-01-10', '2015-11-08', 'Mar123456', 3);
 
 -- Inciso c)
+-- 1)
+select titulo, editorial, edicion, anio_publicacion, case
+when clasificacion = 5 then 'Excelente'
+when clasificacion = 4 then 'Lectura Entretenida'
+when clasificacion = 3 then 'Regular'
+when clasificacion = 2 then 'Evitar'
+when clasificacion = 1 then 'Pésimo'
+when clasificacion = 0 then 'No Recomendado'
+else 'Sin clasificacion'
+end as Clasificacion from libros;
 
+-- 2)
+select concat(nombre, ' ', primer_apellido, ' ', segundo_apellido) as nombre_completo,
+floor(datediff(current_date, date(fecha_nacimiento))/365.25) as edad, fecha_registro,
+case
+	when datediff(current_date, date(fecha_registro))/365.25 < 1 then concat('Recien ', id_rol)
+	when datediff(current_date, date(fecha_registro))/365.25 >= 1 and
+		 datediff(current_date, date(fecha_registro))/365.25 < 3 then concat(id_rol, ' con conocimiento en el sistema')
+	when datediff(current_date, date(fecha_registro))/365.25 >= 3 and
+		 datediff(current_date, date(fecha_registro))/365.25 < 5 then concat(id_rol, ' con experiencia')
+	when floor(datediff(current_date, date(fecha_registro))/365.25) >= 5 then concat(id_rol, ' veterano')
+end as informacion from usuarios;
 
+-- Inciso d)
+-- Obtener la lista completa de todos los usuarios y los libros que han prestado, si es que tienen algún préstamo
+select u.*, l.titulo from usuarios u join prestamos p on u.id_usuario = p.id_usuario
+join libros l on p.id_libro = l.id_libro;
 
+-- Listar todos los libros y los autores correspondientes, incluyendo los libros que no tienen un autor asignado
+select l.*, a.nombre_autor from libros l left join libroautor la on l.id_libro = la.id_libro 
+left join autores a on la.id_autor = a.id_autor;
 
+-- Obtener la lista de todos los autores y los libros que han escrito, si es que han escrito alguno
+select a.*, l.titulo from autores a left join libroautor la on a.id_autor = la.id_autor 
+left join libros l on la.id_libro = l.id_libro;
 
+-- Listar todas las devoluciones junto con los préstamos correspondientes, incluyendo los préstamos que aún no han sido devueltos
+select p.*, d.fecha_devolucion from prestamos p 
+left join devoluciones d on p.id_prestamo = d.id_prestamo;
 
+-- Obtener una lista de usuarios con y sin préstamos, mostrando los detalles de préstamo si existen
+select u.*, p.fecha_prestamo, p.estado_prestamo from usuarios u 
+left join prestamos p on u.id_usuario = p.id_usuario;
 
+-- Listar todas las editoriales y los libros publicados por ellas, incluyendo editoriales que no hayan publicado ningún libro
+select distinct l.editorial, l.titulo from libros l;
 
+-- Obtener una lista de todos los libros prestados, incluyendo los autores de los libros y mostrando también los libros que no han sido prestados
+select l.titulo, a.nombre_autor, p.fecha_prestamo from libros l left join libroautor la on l.id_libro = la.id_libro
+left join autores a on la.id_autor = a.id_autor
+left join prestamos p on l.id_libro = p.id_libro;
 
+-- Listar todos los préstamos realizados y mostrar el nombre del usuario que lo hizo
+select p.*, u.nombre, u.primer_apellido from prestamos p 
+join usuarios u on p.id_usuario = u.id_usuario;
 
+-- Obtener una lista de todos los libros disponibles, incluyendo aquellos sin autor asignado o que no están prestados actualmente
+select l.*, a.nombre_autor from libros l left join libroautor la on l.id_libro = la.id_libro
+left join autores a on la.id_autor = a.id_autor
+left join prestamos p on l.id_libro = p.id_libro and p.estado_prestamo in ('pendiente', 'con retraso')
+where p.id_prestamo is null;
+
+-- Listar todas las multas de los préstamos, incluyendo los usuarios que las generaron, y mostrando préstamos sin multa si los hay
+select p.*, m.monto, u.nombre from prestamos p left join multas m on p.id_prestamo = m.id_prestamo 
+left join usuarios u on p.id_usuario = u.id_usuario;
 
